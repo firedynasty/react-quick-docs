2a072c1654a1f354d18ab7b11fb303fdf2fcb18e
Stanley Tan <stanley.tan19@gmail.com>
2025-05-16 13:49:21 -0700

    faq modal hardcoded

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import AccessiblePDFViewer from './AccessiblePDFViewer';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <AccessiblePDFViewer />
+    </div>
+  );
+}
+
+export default App;
